8edb00737c790e84ee1f11d564ea55277b0185f3
Feng Yang <feng.yang1982@gmail.com>
2020-08-09 16:27:53 -0700

    Added more implementation

--- graph/graph.go
+++ graph/graph.go
@@ -1,5 +1,10 @@
 package graph
 
+import (
+  "bytes"
+  "log"
+)
+
 type Node struct {
   name string
   operation func()
@@ -15,11 +20,17 @@ type Graph struct {
   pipes *[]Pipe
 }
 
+var (
+  buf bytes.Buffer
+  logger = log.New(&buf, "logger: ", log.Lshortfile)
+)
+
 func AddNode(graph *Graph, node Node) bool {
   var i int
   var nodes = (*graph).nodes
   for i = 0; i < len(*nodes); i++ {
     if NodesEqual((*nodes)[i], node) {
+      logger.Printf("Node %+v already exists", node)
       return false
     }
   }
@@ -32,4 +43,8 @@ func AddNode(graph *Graph, node Node) bool {
 
 func NodesEqual(node1 Node, node2 Node) bool {
   return node1.name == node2.name
+}
+
+func PipesEqual(pipe1 Pipe, pipe2 Pipe) bool {
+  return NodesEqual(pipe1.sink, pipe2.sink) && NodesEqual(pipe1.source, pipe2.source)
 }
